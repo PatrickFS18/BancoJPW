ef5639d4aa3d3fcb0aa80fe32d3499d60873624c
PatrickFS18 <Patrick.souza@aluno.riogrande.ifrs.edu.br>
2023-06-23 12:21:11 -0300

    nova logica show saldo

--- resources/views/indexUser.blade.php
+++ resources/views/indexUser.blade.php
@@ -9,24 +9,16 @@
 
 <body>
     <div class="header">
-    <div id="welcomeUser">
-    <p style="font-family: 'Times New Roman', Times, serif; font-size: larger">
-        @php
-        date_default_timezone_set('America/Sao_Paulo'); // Defina o fuso horário desejado
+        <div id="welcomeUser">
+            <p style="font-family: 'Times New Roman', Times, serif; font-size: larger">
+                @php
+                date_default_timezone_set('America/Sao_Paulo'); // Defina o fuso horário desejado
 
 
-            $hour = date('H');
-            if ($hour >= 6 && $hour < 12) {
-                echo 'Bom dia,';
-            } elseif ($hour >= 12 && $hour < 19) {
-                echo 'Boa tarde,';
-            } else {
-                echo 'Boa noite,';
-            }
-        @endphp
-        <span id="spanName">user!</span>
-    </p>
-</div>
+                $hour = date('H');
+                if ($hour >= 6 && $hour < 12) { echo 'Bom dia,' ; } elseif ($hour>= 12 && $hour < 19) { echo 'Boa tarde,' ; } else { echo 'Boa noite,' ; } @endphp <span id="spanName">user!</span>
+            </p>
+        </div>
         <div id="logo-jpw">
             <img src="/img/jpw.png" alt="" id="logo">
         </div>
@@ -45,15 +37,29 @@
         <div id="conteudoBanco">
             <div id="conteudoBancoBackground">
                 <h1 id="titleSaldo">Saldo Disponivel:</h1>
+                
+                @php
+                $saldo="500,00";
+                $saldoInput = "R$****"; // Defina o valor inicial do saldo
+                $toggleButtonClass = "hide-saldo"; // Defina a classe inicial do botão
+
+                if ($saldoInput === "R$****") {
+                $saldoInput = '{{$saldo}}'; // Substitua pelo valor da variável que receberá o valor do saldo
+                $toggleButtonClass = "show-saldo";
+                }
+                @endphp
+
                 <h3 id="saldoInput">R$****</h3>
-                <button type="button" class="btn btn-link" id="toggleButton" onclick="toggleSaldoVisibility()"><i class="fa fa-eye" aria-hidden="true"></i></button>
+                <button type="button" class="btn btn-link {{ $toggleButtonClass }}" id="toggleButton" onclick="toggleSaldoVisibility()"><i class="fa fa-eye" aria-hidden="true"></i></button>
                 <h5 id="limite">Seu limite é: <span id='valorLimite'> R$1000,00</span></h5>
             </div>
 
             <div id="divTransacao">
-                <button type="button" class="btn btn-light"><img src="/img/pix.png" id ='pixIcon'alt="">pix</button>
-                <button type="button" class="btn btn-light"><img src="/img/pagar.png" id ='payIcon'alt="">Pagar</button>
-                <button type="button" class="btn btn-light" id="transferButton"><img src="/img/transferir.png" id ='transferIcon'alt=""><p id="transferButtonP">Transferir</p></button>
+                <button type="button" class="btn btn-light"><img src="/img/pix.png" id='pixIcon' alt="">pix</button>
+                <button type="button" class="btn btn-light"><img src="/img/pagar.png" id='payIcon' alt="">Pagar</button>
+                <button type="button" class="btn btn-light" id="transferButton"><img src="/img/transferir.png" id='transferIcon' alt="">
+                    <p id="transferButtonP">Transferir</p>
+                </button>
             </div>
         </div>
         <div>
@@ -65,21 +71,20 @@
     <script src="https://kit.fontawesome.com/39394938ab.js" crossorigin="anonymous"></script>
     <script src="https://use.fontawesome.com/34480fc0c2.js"></script>
     <script>
-        function toggleSaldoVisibility() {
-            var saldoInput = document.getElementById("saldoInput");
-            var toggleButton = document.getElementById("toggleButton");
+      function toggleSaldoVisibility() {
+        var saldoInput = document.getElementById("saldoInput");
+        var toggleButton = document.getElementById("toggleButton");
 
-            if (saldoInput.textContent === "R$****") {
-                saldoInput.textContent = "R$50,00";
-                //Patrick no lugar no 50,00 tu insere a variavel que irá receber o valor do saldo
-                toggleButton.classList.remove("hide-saldo");
-                toggleButton.classList.add("show-saldo");
-            } else {
-                saldoInput.textContent = "R$****";
-                toggleButton.classList.remove("show-saldo");
-                toggleButton.classList.add("hide-saldo");
-            }
+        if (saldoInput.textContent === "R$****") {
+            saldoInput.textContent = "R$50,00";
+            toggleButton.classList.remove("btn btn-link hide-saldo");
+            toggleButton.classList.add("btn btn-link show-saldo");
+        } else {
+            saldoInput.textContent = "R$****";
+            toggleButton.classList.remove("btn btn-link show-saldo");
+            toggleButton.classList.add("btn btn-link hide-saldo");
         }
+    }
     </script>
 </body>
 
