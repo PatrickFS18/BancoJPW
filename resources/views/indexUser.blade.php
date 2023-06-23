a7d86b54ec3fab6e34389ab38280443f67ae2706
PatrickFS18 <Patrick.souza@aluno.riogrande.ifrs.edu.br>
2023-06-23 11:00:19 -0300

    link css e js

--- resources/views/indexUser.blade.php
+++ resources/views/indexUser.blade.php
@@ -4,12 +4,12 @@
 <head>
     <title>Meu Banco</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="indexUser.css">
+    <link rel="stylesheet" href="{{ asset('css/indexUser.css') }}">
 </head>
 
 <body>
     <div class="header">
-        <div id="welcomeUser">Bem vindo <span id="spanName">{{user}}</span>
+        <div id="welcomeUser">Bem vindo <span id="spanName">user</span>
         </div>
         <div id="logo-jpw">
             <img src="/img/jpw.png" alt="" id="logo">
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\LoginController;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('indexUser');
 });
 Route::get('/home', function () {
     return view('home');
